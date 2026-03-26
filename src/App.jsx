4888c31dac5365f8ce8df0cd8e5ba59a811b768b
Djenica <djenicaexilus@gmail.com>
2026-03-26 13:25:57 -0400

    Update EmployeeForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import EmployeeForm from "./components/EmployeeForm";
 
 function App() {
-  return <></>;
+
+  return(
+    <div>
+      <h1>Form</h1>
+      <EmployeeForm />
+    </div>
+  )
 }
 
 export default App;
